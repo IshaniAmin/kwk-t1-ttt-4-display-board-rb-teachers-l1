52189852028c4ab2cd66cb420509de0b044535da
Ishani <ishaniamin@gmail.com>
2018-05-29 17:12:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,5 +4,9 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(board)
-
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
